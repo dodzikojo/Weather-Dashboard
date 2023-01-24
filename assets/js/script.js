97a018950822df0c60ef1bc305702e49e27d4ade
dodzikojo <dodzi@windowslive.com>
2023-01-24 21:47:57 +0000

    Added functionality to search list of cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,57 @@
 
 
 const APIKey = "cec087f788b4b894cba40f0ebde76d37"
-let openWeatherMapAPI = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid="+APIKey;
+let openWeatherMapAPI = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
 
-let geoCodingAPI = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid="+APIKey
+let geoCodingAPI = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + APIKey
 
+let allPossibleCities = [];
 
 var moment = require("moment")
-var citites = require("cities.json")
-
-console.log(moment)
-console.log(citites)
+var cities = require("cities.json")
 
 testGeoCoding()
 
 
-function testGeoCoding(){
-    $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid="+APIKey, successFn)
+
+
+function testGeoCoding() {
+    $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" + APIKey, successFn)
 }
 
 
 function successFn(result) {
-    console.log("Result: "+result)
+    console.log("Result: " + result)
 }
 
 
 $("form").submit(function (event) {
     event.preventDefault();
-
-    console.log("form submitted");
+    
 })
+
+
+
+
+$("#searchBar").keyup(function (e) {
+    allPossibleCities = []
+    if ($("#searchBar").val().length >= 2) {
+        for (let index = 0; index < cities.length; index++) {
+
+            if (cities[index].name.toLowerCase() == $("#searchBar").val().toLowerCase() || cities[index].name.toLowerCase().includes($("#searchBar").val().toLowerCase())) {
+                // console.log(cities[index]);
+                allPossibleCities.push(cities[index].name + ", " + cities[index].country)
+            }
+        }
+
+    }
+
+    $("#searchBar").autocomplete({
+        maxHeight: 10,
+        source: allPossibleCities.filter(onlyUnique).slice(0,9)
+    });
+});
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
