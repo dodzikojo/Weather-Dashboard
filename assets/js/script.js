260c6fd900193fe13624c739ba851b59571a71e3
dodzikojo <dodzi@windowslive.com>
2023-01-25 07:05:15 +0000

    Added support to request weatherdata

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,51 @@
 
 
-const APIKey = "cec087f788b4b894cba40f0ebde76d37"
+const APIKey = "234dd37d46a1b4a430f58520a10e1039"
+
 let openWeatherMapAPI = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
 
 let geoCodingAPI = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + APIKey
+
+let sampleAPI = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
 
 let allPossibleCities = [];
 
 var moment = require("moment")
 var cities = require("cities.json")
 
-testGeoCoding()
 
 
+// testGeoCoding("accra") 
+// testGeoCoding(1, 1)
 
+getLocation()
 
-function testGeoCoding() {
-    $.getJSON("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" + APIKey, successFn)
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
 }
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude);
+    console.log("Longitude: " + position.coords.longitude);
+}
+
+
+
+function testGeoCoding(lat, long) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + APIKey,
+        method: "GET"
+    }).then(function (response) {
+        response.list.forEach(element => {
+            console.log(element);
+        });
+    });
+}
+
 
 
 function successFn(result) {
@@ -27,7 +55,24 @@ function successFn(result) {
 
 $("form").submit(function (event) {
     event.preventDefault();
-    
+    var array = $('#searchBar').val().split(', ');
+    console.log(array)
+
+    for (let index = 0; index < cities.length; index++) {
+        if (cities[index].name.toLowerCase() === array[0].toLowerCase().trim() && cities[index].country.toLowerCase() === array[1].toLowerCase().trim()) {
+            console.log(cities[index]);
+
+            // testGeoCoding(cities[index].name);
+            // console.log(testGeoCoding(cities[index].name))
+            console.log(cities[index].lat + "," + cities[index].lng)
+            testGeoCoding(cities[index].lat, cities[index].lng)
+            break;
+        }
+
+    }
+
+
+    // testGeoCoding(array[0].trim());
 })
 
 
@@ -39,7 +84,7 @@ $("#searchBar").keyup(function (e) {
         for (let index = 0; index < cities.length; index++) {
 
             if (cities[index].name.toLowerCase() == $("#searchBar").val().toLowerCase() || cities[index].name.toLowerCase().includes($("#searchBar").val().toLowerCase())) {
-                // console.log(cities[index]);
+                // console.log(cities[index])
                 allPossibleCities.push(cities[index].name + ", " + cities[index].country)
             }
         }
@@ -48,10 +93,15 @@ $("#searchBar").keyup(function (e) {
 
     $("#searchBar").autocomplete({
         maxHeight: 10,
-        source: allPossibleCities.filter(onlyUnique).slice(0,9)
+        source: allPossibleCities.filter(onlyUnique).slice(0, 9)
     });
 });
 
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
+
+// $( "#searchBar" ).on( "autocompleteselect", function( event, ui ) {
+//     console.log($('#searchBar').val())
+// } );
+
