24d792efde9378be635d5fd0318139a05eaf30c3
dodzikojo <dodzi@windowslive.com>
2023-01-25 11:43:59 +0000

    app now updates temperature, description html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,75 @@
-
-
 const APIKey = "234dd37d46a1b4a430f58520a10e1039"
-
-let openWeatherMapAPI = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
-
-let geoCodingAPI = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + APIKey
-
-let sampleAPI = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-
 let allPossibleCities = [];
+let moment = require("moment")
+let cities = require("cities.json")
 
-var moment = require("moment")
-var cities = require("cities.json")
+let locationName = $("#locationName");
+let temperature = $("#temperature");
+var mainDescription = $("#mainDescription");
+var secondaryDescription = $("#secondaryDescription");
 
+let mainWeatherData;
 
+// getJSONWeatherData(5.55602, -0.1969)
 
-// testGeoCoding("accra") 
-// testGeoCoding(1, 1)
+function getJSONWeatherData(lat, lon) {
+    var openWeatherMapAPI = "https://api.openweathermap.org/data/2.5/forecast";
 
-getLocation()
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
+    $.getJSON(openWeatherMapAPI, {
+        lat: lat,
+        lon: lon,
+        APIKey: APIKey
+    }, successFn)
 }
-
-function showPosition(position) {
-    console.log("Latitude: " + position.coords.latitude);
-    console.log("Longitude: " + position.coords.longitude);
-}
-
-
-
-function testGeoCoding(lat, long) {
-    $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + APIKey,
-        method: "GET"
-    }).then(function (response) {
-        response.list.forEach(element => {
-            console.log(element);
-        });
-    });
-}
-
-
 
 function successFn(result) {
-    console.log("Result: " + result)
+    mainWeatherData  = result;
+
+    updateHTMLElements();
+
+    result.list.forEach(element => {
+        console.log(element)
+    });
+
+    console.log(result.list[0].weather[0].main)
+    
 }
+
+
+
+//#region  Get current location
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         console.log("Geolocation is not supported by this browser.");
+//     }
+// }
+
+// function showPosition(position) {
+//     console.log("Latitude: " + position.coords.latitude);
+//     console.log("Longitude: " + position.coords.longitude);
+// }
+//#endregion
+
+
+
+
+
 
 
 $("form").submit(function (event) {
     event.preventDefault();
     var array = $('#searchBar').val().split(', ');
-    console.log(array)
-
     for (let index = 0; index < cities.length; index++) {
         if (cities[index].name.toLowerCase() === array[0].toLowerCase().trim() && cities[index].country.toLowerCase() === array[1].toLowerCase().trim()) {
-            console.log(cities[index]);
-
-            // testGeoCoding(cities[index].name);
-            // console.log(testGeoCoding(cities[index].name))
-            console.log(cities[index].lat + "," + cities[index].lng)
-            testGeoCoding(cities[index].lat, cities[index].lng)
+            getJSONWeatherData(cities[index].lat, cities[index].lng)
+            locationName.text(cities[index].name + ", " + cities[index].country)
             break;
         }
 
     }
-
-
-    // testGeoCoding(array[0].trim());
+    
 })
 
 
@@ -84,11 +81,9 @@ $("#searchBar").keyup(function (e) {
         for (let index = 0; index < cities.length; index++) {
 
             if (cities[index].name.toLowerCase() == $("#searchBar").val().toLowerCase() || cities[index].name.toLowerCase().includes($("#searchBar").val().toLowerCase())) {
-                // console.log(cities[index])
                 allPossibleCities.push(cities[index].name + ", " + cities[index].country)
             }
         }
-
     }
 
     $("#searchBar").autocomplete({
@@ -101,7 +96,16 @@ function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
-// $( "#searchBar" ).on( "autocompleteselect", function( event, ui ) {
-//     console.log($('#searchBar').val())
-// } );
+function updateHTMLElements () {
+    temperature.text(convertKelvinToCelsius(mainWeatherData.list[0].main.temp).toFixed(0));
+    mainDescription.text(mainWeatherData.list[0].weather[0].main);
+    secondaryDescription.text("("+mainWeatherData.list[0].weather[0].description+")");
+}
 
+function convertKelvinToFahrenheit(valueToConvert){
+    return ((valueToConvert-273.15)*1.8)+32
+}
+
+function convertKelvinToCelsius(valueToConvert){
+    return (valueToConvert-273.15)
+}
